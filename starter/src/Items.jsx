3959458319bac1f28cd517a2171671d3190afe93
James Volmert <james.volmert@gmail.com>
2024-08-07 16:19:38 -0600

    post request

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -2,14 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import SingleItem from "./SingleItem";
 import fetchRequest from "./utils/utils";
 const Items = ({ items }) => {
-  const res = useQuery({
+  const { data, error, isLoading, isError } = useQuery({
     queryKey: ["tasks"],
     queryFn: () => fetchRequest.get("/"),
   });
-  console.log(res);
+  console.log(data);
+  if (isLoading) return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  if (isError) {
+    return <p style={{ marginTop: "1rem" }}>Error occurred: {error}</p>;
+  }
   return (
     <div className="items">
-      {items.map((item) => {
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
