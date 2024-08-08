e1ea6e31d95a3e0da42c94c5e72c2805cb8a36ee
James Volmert <james.volmert@gmail.com>
2024-08-07 18:22:57 -0600

    edit and delte task with react query

--- starter/src/SingleItem.jsx
+++ starter/src/SingleItem.jsx
@@ -1,23 +1,51 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import fetchRequest from "./utils/utils";
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return fetchRequest.patch(`/${taskId}`, {
+        isDone,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: (taskId) => {
+      return fetchRequest.delete(`/${taskId}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
   return (
-    <div className='single-item'>
+    <div className="single-item">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() =>
+          editTask({
+            taskId: item.id,
+            isDone: !item.isDone,
+          })
+        }
       />
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
         }}
       >
         {item.title}
       </p>
       <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
+        className="btn remove-btn"
+        type="button"
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
